7eb9a877f84474f7c55f896b68532ce1615d05d4
iNeverNobody <106006452+iNeverNobody@users.noreply.github.com>
2022-08-24 16:05:10 +0800

    Hello World!!!

--- Hello world.cpp
+++ Hello world.cpp
@@ -3,3 +3,15 @@ using namespace std;
 int main(){
     cout<<"Hello World!!!";
 }
+/*
+#include <stdio.h>
+int main(){
+    printf("Hello World!!!");
+}
+*/
+/*
+#include <ctsdio>
+int main(){
+    printf("Hello World!!!");
+}
+*/
