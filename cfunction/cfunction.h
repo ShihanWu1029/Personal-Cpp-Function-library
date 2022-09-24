a0c7e2351cf977ff53646808be37588c99b5a126
AndyWu <106006452+iNeverNobody@users.noreply.github.com>
2022-09-24 11:43:08 +0800

    Version 1.0.6.6-1(formal)
    紧急修复更改类名后忘该构造函数与参数的bug

--- cfunction/cfunction.h
+++ cfunction/cfunction.h
@@ -47,18 +47,18 @@ template<class XType,class YType>class c_crdinate{			//用于构建坐标系
 		long double tmp_betw=0;
 		long double err_return=0.00;
 	public:
-		crdinate(void){		}
-		crdinate(XType __Give_X,YType __Give_Y){this->__Xpos=__Give_X;this->__Ypos=__Give_Y;}
+		c_crdinate(void){		}
+		c_crdinate(XType __Give_X,YType __Give_Y){this->__Xpos=__Give_X;this->__Ypos=__Give_Y;}
 		void set(XType __Give_X,YType __Give_Y){this->__Xpos=__Give_X;this->__Ypos=__Give_Y;}
 		void set_X(XType __Give_X){this->__Xpos=__Give_X;}
 		void set_Y(YType __Give_Y){this->__Ypos=__Give_Y;}
 		inline XType get_X(void){return this->__Xpos;}
 		inline YType get_Y(void){return this->__Ypos;}
-		crdinate get(void){return *this;}
-		inline long double distance_between(crdinate __other){tmp_betw=sqrt((this->__Xpos-__other.__Xpos)*(this->__Xpos-__other.__Xpos)+(this->__Ypos-__other.__Ypos)*(this->__Ypos-__other.__Ypos));return tmp_betw;}
-		inline bool operator==(const crdinate __anthr){return (this->__Xpos==__anthr.__Xpos && this->__Ypos==__anthr.__Ypos);}
-		inline bool operator<=(const crdinate __anthr){return (this->__Xpos<=__anthr.__Xpos && this->__Ypos<=__anthr.__Ypos);}
-		inline bool operator>=(const crdinate __anthr){return (this->__Xpos>=__anthr.__Xpos && this->__Ypos>=__anthr.__Ypos);}
+		c_crdinate get(void){return *this;}
+		inline long double distance_between(c_crdinate __other){tmp_betw=sqrt((this->__Xpos-__other.__Xpos)*(this->__Xpos-__other.__Xpos)+(this->__Ypos-__other.__Ypos)*(this->__Ypos-__other.__Ypos));return tmp_betw;}
+		inline bool operator==(const c_crdinate __anthr){return (this->__Xpos==__anthr.__Xpos && this->__Ypos==__anthr.__Ypos);}
+		inline bool operator<=(const c_crdinate __anthr){return (this->__Xpos<=__anthr.__Xpos && this->__Ypos<=__anthr.__Ypos);}
+		inline bool operator>=(const c_crdinate __anthr){return (this->__Xpos>=__anthr.__Xpos && this->__Ypos>=__anthr.__Ypos);}
 		inline XType operator[](int __get){if(__get>2 || __get<0) return 0;if(__get==0) return this->__Xpos;else return this->__Ypos;}
 };
 
@@ -80,7 +80,7 @@ class c_counter{
 	private:
 		unsigned long long __inCounterr;
 	public:
-		counter(){this->__inCounterr=0;}
+		c_counter(){this->__inCounterr=0;}
 		inline void counter_reset(void){this->__inCounterr=0;}	//重置计数器为0 
 		inline void counter_startAt(unsigned long long __startAt){this->__inCounterr=__startAt;}		//设置计数器为输入的数 
 		inline void counter_plus(void){this->__inCounterr++;}	//将计数器加一
@@ -94,8 +94,8 @@ template<class type>class c_list{
 		unsigned __SSize=0;
 	public:
 		bool isSet=false;
-		list(){		}
-		list(int __size){this->__a=new type[__size+1];isSet=true;this->__SSize=__size;}
+		c_list(){		}
+		c_list(int __size){this->__a=new type[__size+1];isSet=true;this->__SSize=__size;}
 		void set_length(int __size){if(!isSet){this->__a=new type[__size+1];isSet=true;this->SSize=__size;}}
 		inline type operator[](const unsigned __at){return this->__a[__at];}
 		inline void set(type __at,type __value=0){this->__a[__at]=__value;}
@@ -109,9 +109,9 @@ class str{
 		string __inStr="";
 	public:
 		unsigned long long npos;
-		str(){this->npos=this->__inStr.npos;}
-		str(const string __give){this->__inStr=__give;this->npos=this->__inStr.npos;}
-		str(const string __give,const int __times){for(int __ii=0;__ii<__times;__ii++)for(int __jj=0;__jj<__give.size();__jj++)this->__inStr.push_back(__give[__jj]);}
+		c_str(){this->npos=this->__inStr.npos;}
+		c_str(const string __give){this->__inStr=__give;this->npos=this->__inStr.npos;}
+		c_str(const string __give,const int __times){for(int __ii=0;__ii<__times;__ii++)for(int __jj=0;__jj<__give.size();__jj++)this->__inStr.push_back(__give[__jj]);}
 		inline void refresh_npos(void){this->npos=this->__inStr.npos;}
 		inline void set(const string __given){this->__inStr=__given;}
 		string get(void){return this->__inStr;}
@@ -148,10 +148,10 @@ template<class __type1,class __type2>class c_dictionary{//字典
 	public:
 		unsigned size=0;//存字典大小
 		unsigned MAX_SIZE=0;//存最大大小 
-		dictionary(){this->name=new __type1[10000];this->data=new __type2[10000];MAX_SIZE=10000;}//默认大小10000 
-		dictionary(int __make_size){this->name=new __type1[__make_size];this->data=new __type2[__make_size];MAX_SIZE=__make_size;}//构造 
+		c_dictionary(){this->name=new __type1[10000];this->data=new __type2[10000];MAX_SIZE=10000;}//默认大小10000 
+		c_dictionary(int __make_size){this->name=new __type1[__make_size];this->data=new __type2[__make_size];MAX_SIZE=__make_size;}//构造 
 		void make_word(__type1 __Name,__type2 __Data){this->name[size]=__Name;this->data[size++]=__Data;}//添加成员 
-		void append_dictionary(dictionary __rec){int __lenof=__rec.size;for(int i=0;i<__lenof;i++){if(size>=MAX_SIZE){cout<<this->__overloadError_msg;return;}this->make_word(__rec.get_name(i),__rec.at_subscript(i));}}//用于在字典中添加另一字典类 
+		void append_dictionary(c_dictionary __rec){int __lenof=__rec.size;for(int i=0;i<__lenof;i++){if(size>=MAX_SIZE){cout<<this->__overloadError_msg;return;}this->make_word(__rec.get_name(i),__rec.at_subscript(i));}}//用于在字典中添加另一字典类 
 		__type2 operator[](const __type1 __at){long long __res_of_searching=this->__searching_in_dictionary(__at);if(__res_of_searching==-1)  cout<<this->__noFounError_msg;else return this->data[__res_of_searching];}//重载下标运算符 
 		__type2 at(const __type1 __at){return this->operator[](__at);}//at函数 
 		__type2 at_subscript(const int __at){if(__at>=size){return __type2(0);}return this->data[__at];}//通过下标找元素 
@@ -161,13 +161,13 @@ template<class __type1,class __type2>class c_dictionary{//字典
 		istream &read(istream &is,const unsigned __tag_size,const bool __colon_input=false){int __tag=(__tag_size+size>MAX_SIZE?MAX_SIZE:__tag_size);char rec_colon;__type1 __rec1;__type2 __rec2;for(int i=size;i<__tag;i++){if(__colon_input) is>>__rec1>>rec_colon>>__rec2;else is>>__rec1>>__rec2;this->make_word(__rec1,__rec2);}}
 		void clear(void){delete this->name;delete this->data;delete this;}
 };
-template<typename _T1,typename _T2>dictionary<_T1,_T2> make_dictionary(_T1 __name,_T2 __data){
-	dictionary<_T1,_T2> __res(1);
+template<typename _T1,typename _T2>c_dictionary<_T1,_T2> make_dictionary(_T1 __name,_T2 __data){
+	c_dictionary<_T1,_T2> __res(1);
 	__res.make_word(__name,__data);
 	return __res;
 }//用于制造字典
-template<typename _T1,typename _T2>dictionary<_T1,_T2> make_dictionary(_T1 __name,_T2 __data,_T1 __name1,_T2 __data1){
-	dictionary<_T1,_T2> __res(2);
+template<typename _T1,typename _T2>c_dictionary<_T1,_T2> make_dictionary(_T1 __name,_T2 __data,_T1 __name1,_T2 __data1){
+	c_dictionary<_T1,_T2> __res(2);
 	__res.make_word(__name,__data);
 	__res.make_word(__name1,__data1);
 	return __res;
