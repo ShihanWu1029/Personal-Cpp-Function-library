dc8d89ffc36d7a2b5f93668af1101e7061a84c73
AndyWu <106006452+iNeverNobody@users.noreply.github.com>
2022-10-16 15:26:56 +0800

    WHos Unfinished

--- WHos/main.cpp
+++ WHos/main.cpp
@@ -13,6 +13,21 @@
 
 using namespace std;
 
+
+class error_t{
+	private:
+		string defaultString = "[Error]";
+		string whats="Unexpected runtime error!";
+		bool error=false;
+	public:
+		error_t (void){}
+		error_t (const string __what){this->whats = __what;this->error=true;}
+		string what(void){return this->whats;}
+		bool isError(void){return this->error;} 
+		operator string() {return this->whats;} 
+};
+
+
 template <typename type1 , typename type2> class user{
 	private: vector<pair<type1,type2>> __vp;
 	public:
@@ -28,40 +43,40 @@ template <typename type1 , typename type2> class user{
 		}
 };
 
+
 ostream &print(const string text,ostream &istrm=cout){
 	ios::sync_with_stdio(false);
 	for(int i=0;i<text.size();i++){
-		istrm<<text[i]<<flush;
+		if(istrm<<text[i]<<flush);
+		else throw error_t("[Error] Exception happens when output!");
 		Sleep(10); 
 	}
 	return istrm;
 //	cout<<endl;
 } 
 
+
+
 class cmd{
 	private:
 		stack<string> rec_cmd;
 	public:
+		string bases;
 		cmd(void) = default;
 		operator ()(const string __what){
 			return system(__what.c_str());
 		}
+		operator ()(void){
+			return system(bases.c_str());
+		}
+		istream &readd(){
+			return (cin>>this->bases);
+		}
 };
 
-class error_t{
-	private:
-		string defaultString = "[Error]";
-		string whats="Unexpected runtime error!";
-		bool error=false;
-	public:
-		error_t (void){}
-		error_t (const string __what){this->whats = __what;this->error=true;}
-		string what(void){return this->whats;}
-		bool isError(void){return this->error;} 
-		operator string() {return this->whats;} 
-};
 
-int login(void) noexcept{
+
+int login(void){
 	ios::sync_with_stdio(false);
 	user<string,string> user_list={
 		{"admin","administrator",},
@@ -77,9 +92,19 @@ int login(void) noexcept{
 	int cnt=0;
 	while(cnt<=10){
 		print("Username: >");
-		cin>>*usn;
+		if(cin>>*usn);
+		else{
+			delete usn;
+			delete psw;
+			throw error_t("[Error] Excepted input type!");
+		}	
 		print("Password: >");
-		cin>>*psw;
+		if(cin>>*psw);
+		else{
+			delete usn;
+			delete psw;
+			throw error_t("[Error] Excepted input type!");
+		}
 		if(user_list.check(*usn,*psw)){
 			break;
 		}else{
@@ -87,19 +112,33 @@ int login(void) noexcept{
 		}
 		cnt++;
 	}
-	if(! cnt<=10) return -1;
+	if(cnt>=10) return -1;
 	print("Login successfully!\nWelcome ");
 	print(*usn);
 	print("."); 
 	return ((*usn)=="admin"?0:1);
 }
 cmd main_cmd;
+bool isAdmin=false;
 int main(){
-	int get=login();
+	int get;
+	try{
+		get=login();
+	}catch(error_t error){
+		cerr<<error.what()<<endl;
+		return 0;
+	}
 	if(get==-1){
 		print("\nToo more time to try!The system will exit in 3second!Bye.");
 		Sleep(3000); 
 		return 0;
+	}else{
+		isAdmin=(get==0?true:false);
+//		cout<<boolalpha<<isAdmin<<noboolalpha;
+		while(true){
+			//main code here!
+			
+		}
 	}
 	
 	return 0;
